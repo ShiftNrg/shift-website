041ae743b671577d0cb6e8fc1ea2b33ae3a5e398
LucasH <LucasH@localhost>
2019-07-04 08:59:25 +0200

    setting all scenes and layers, to make page switching possible later on.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,3 +2,13 @@ import '../sass/styles.scss';
 
 // Remove "no-js" class since we do have javascript enabled
 document.body.className = document.body.className.replace("no-js","");
+
+
+// Set the --vh without the navbar for mobile
+function calculateHeight()
+{
+    var vh = window.innerHeight;
+    document.documentElement.style.setProperty('--vh', vh + "px");
+}
+window.addEventListener('resize', calculateHeight);
+calculateHeight();
